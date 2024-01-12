29ce4ce4d87aee7a30a7cf6e68ecd1ff5475cb61
faijulislambd <f.faijulislam144@gmail.com>
2024-01-13 00:26:01 +0600

    Cards Added

--- src/components/PublicDataTab/PublicDataTab.jsx
+++ src/components/PublicDataTab/PublicDataTab.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import Datepicker from "react-tailwindcss-datepicker";
+import {
+  MdOutlineSentimentSatisfied,
+  MdOutlineSentimentDissatisfied,
+  MdOutlineSentimentNeutral,
+} from "react-icons/md";
 
 const PublicDataTab = () => {
   const [value, setValue] = useState({
@@ -12,8 +17,92 @@ const PublicDataTab = () => {
     setValue(newValue);
   };
 
+  const [todayCount, setTodayCount] = useState(true);
+  const [yesterdayCount, setYesterdayCount] = useState(false);
+  const [weeklyCount, setWeeklyCount] = useState(false);
+  const [monthlyCount, setMonthlyCount] = useState(false);
+
   return (
     <div>
+      <div className="relative flex items-center justify-center -mx-[15px] mb-8 px-15 pt-14 pb-5 border-2 border-sky-500 rounded-md shadow-lg before:content-['Troubleshooting_Count'] before:text-black before:absolute before:left-2 before:-top-2.5 before:bg-white before:text-xs before:px-1 before:rounded before:border-sky-500 before:border-2">
+        <ul className="flex justify-center items-center space-x-3 absolute left-1/2 -top-6 -translate-x-1/2 z-10">
+          <li
+            className={`text-lg text-nowrap uppercase px-4 py-2 shadow-sm border-2 rounded-md border-sky-500 cursor-pointer transition-all duration-150 ${
+              todayCount
+                ? "bg-sky-500 text-white"
+                : "hover:bg-sky-500 hover:text-white bg-white"
+            }`}
+            onClick={() => {
+              (todayCount === false) & setTodayCount(true);
+            }}
+          >
+            Today's Count
+          </li>
+          <li
+            className={`text-lg text-nowrap uppercase px-4 py-2 shadow-sm border-2 rounded-md border-sky-500 cursor-pointer transition-all duration-150 ${
+              yesterdayCount
+                ? "bg-sky-500 text-white"
+                : "hover:bg-sky-500 hover:text-white bg-white"
+            }`}
+            onClick={() => {
+              (yesterdayCount === false) & setYesterdayCount(true);
+            }}
+          >
+            Yesterday's Count
+          </li>
+          <li
+            className={`text-lg text-nowrap uppercase px-4 py-2 shadow-sm border-2 rounded-md border-sky-500 cursor-pointer transition-all duration-150 ${
+              weeklyCount
+                ? "bg-sky-500 text-white"
+                : "hover:bg-sky-500 hover:text-white bg-white"
+            }`}
+            onClick={() => {
+              (weeklyCount === false) & setWeeklyCount(true);
+            }}
+          >
+            Weekly Count
+          </li>
+          <li
+            className={`text-lg text-nowrap uppercase px-4 py-2 shadow-sm border-2 rounded-md border-sky-500 cursor-pointer transition-all duration-150 ${
+              monthlyCount
+                ? "bg-sky-500 text-white"
+                : "hover:bg-sky-500 hover:text-white bg-white"
+            }`}
+            onClick={() => {
+              (monthlyCount === false) & setMonthlyCount(true);
+            }}
+          >
+            Monthly Count
+          </li>
+        </ul>
+        <div className="w-1/3 px-[15px]">
+          <div className="shadow-lg rounded-lg p-8 bg-lime-400 relative z-auto transition-all hover:scale-105">
+            <MdOutlineSentimentSatisfied className="absolute left-3 text-[10rem] top-1/2 -translate-y-1/2 text-[rgba(0,0,0,.3)]" />
+            <h3 className="absolute right-5 top-3 uppercase text-lg font-bold text-shadow-inner text-[rgba(0,0,0,.5)]">
+              Resolved Issues
+            </h3>
+            <div className="text-[5rem] text-right z-1">110</div>
+          </div>
+        </div>
+        <div className="w-1/3 px-[15px]">
+          <div className="shadow-lg rounded-lg p-8 bg-rose-500 relative z-auto transition-all hover:scale-105">
+            <MdOutlineSentimentDissatisfied className="absolute left-3 text-[10rem] top-1/2 -translate-y-1/2 text-[rgba(0,0,0,.3)]" />
+            <h3 className="absolute right-5 top-3 uppercase text-lg font-bold text-shadow-inner text-[rgba(0,0,0,.5)]">
+              Pending Issues
+            </h3>
+            <div className="text-[5rem] text-right z-1">110</div>
+          </div>
+        </div>
+        <div className="w-1/3 px-[15px]">
+          <div className="shadow-lg rounded-lg p-8 bg-amber-200 relative z-auto transition-all hover:scale-105">
+            <MdOutlineSentimentNeutral className="absolute left-3 text-[10rem] top-1/2 -translate-y-1/2 text-[rgba(0,0,0,.3)]" />
+            <h3 className="absolute right-5 top-3 uppercase text-lg font-bold text-shadow-inner text-[rgba(0,0,0,.5)]">
+              In-Progress
+            </h3>
+            <div className="text-[5rem] text-right z-1">110</div>
+          </div>
+        </div>
+      </div>
       <div className="relative flex items-center justify-center -mx-[15px] mb-8 px-15 py-4 border-2 border-sky-500 rounded-md shadow-lg before:content-['Filter_Table'] before:text-black before:absolute before:left-2 before:-top-2.5 before:bg-white before:text-xs before:px-1 before:rounded before:border-sky-500 before:border-2">
         <div className="w-1/6 px-[15px]">
           <label className="block text-sm font-medium leading-6 text-gray-900">
